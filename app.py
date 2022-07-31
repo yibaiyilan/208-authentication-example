a80bd0ba68ced2de593f3acca6b60a7a9fb1bf14
Xiaobei <yibaiyilan@gmail.com>
2022-07-31 00:14:15 +0000

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly
 
 # Keep this out of source code repository - save in a file or a database
 VALID_USERNAME_PASSWORD_PAIRS = {
-    'Mickey': 'Mouse', 'Donald': 'Duck'
+    'Magic': 'Happens', 'Keep': 'Calm', 'Carry': 'On'
 }
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -21,15 +21,16 @@ auth = dash_auth.BasicAuth(
 )
 
 app.layout = html.Div([
-    html.H1('Welcome to the app'),
+    html.H1('Welcome to the Magic World),
     html.H3('You are successfully authorized'),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in [1, 2, 3, 4, 5]],
         value=1
     ),
+    html.Div(id='graph-title'),
     dcc.Graph(id='graph'),
-    html.A('Code on Github', href='https://github.com/austinlasseter/dash-auth-example'),
+    html.A('Code on Github', href='https://github.com/yibaiyilan/208-authentication-example'),
     html.Br(),
     html.A("Data Source", href='https://dash.plotly.com/authentication'),
 ], className='container')
@@ -38,21 +39,40 @@ app.layout = html.Div([
     dash.dependencies.Output('graph', 'figure'),
     [dash.dependencies.Input('dropdown', 'value')])
 def update_graph(dropdown_value):
-    x_values = [-3,-2,-1,0,1,2,3]
-    y_values = [x**dropdown_value for x in x_values]
+    x = np.linspace(-5,5,100)
+    y = x**
 
-    return {
-        'layout': {
-            'title': 'Graph of {}'.format(str(dropdown_value)),
-            'margin': {
-                'l': 20,
-                'b': 20,
-                'r': 10,
-                't': 60
-            }
-        },
-        'data': [{'x': x_values, 'y': y_values}]
-    }
+    mu, sigma = 0, 0.1 # mean and standard deviation
+
+    # Build figure
+    fig = go.Figure()
+    graph_title='Graph of {}'.format(str(dropdown_value))
+    # Add scatter trace with medium sized markers
+    fig.add_trace(
+        go.Scatter(
+            mode='markers',
+            x=x,
+            y=y,
+            marker=dict(
+                color='LightSkyBlue',
+                size=20,
+                opacity=0.5,
+                line=dict(
+                    color='MediumPurple',
+                    width=2
+                )
+            ),
+            showlegend=False
+        )
+    )
+
+    fig.update_layout(
+        margin=dict(l=20, r=20, t=20, b=20),
+        paper_bgcolor="LightSteelBlue",
+    )
+    fig.update_layout(width=400,height=400)
+
+    return fig,graph_title
 
 ############ Deploy
 if __name__ == '__main__':
